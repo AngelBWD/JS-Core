ecbccf667907ca0d5487d925df3aff4d424b6d86
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2019-05-19 22:16:12 +0300

    Add files via upload

---
+++ JS Essentials - май 2019/17.05.2019 OBJECTS AND DOM/08. Visited Websites/solution.js
@@ -0,0 +1,20 @@
+function solve() {
+
+  let siteElements = document.getElementsByClassName('link-1');
+
+  for (let i = 0; i < siteElements.length; i++) {
+    siteElements[i].addEventListener('click', (e) => {
+      let currentTarget = e.currentTarget;
+      let paragraphElement = currentTarget.getElementsByTagName('p')[0];
+
+      let text = paragraphElement.textContent;
+      let textParts = text.split(' ');
+      let clicks = Number(textParts[1]);
+      clicks++;
+
+      textParts[1] = clicks;
+      paragraphElement.textContent = textParts.join(' ');
+
+    });
+  }
+}
