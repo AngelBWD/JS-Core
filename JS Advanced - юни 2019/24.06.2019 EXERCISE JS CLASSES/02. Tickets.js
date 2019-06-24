232521441d0fc56cf98ce3cb37cd3be1e6d91f3f
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2019-06-25 00:13:30 +0300

    Add files via upload

---
+++ JS Advanced - юни 2019/24.06.2019 EXERCISE JS CLASSES/02. Tickets.js
@@ -0,0 +1,46 @@
+function solve(ticketArray, sortingCriteria) {
+
+    class Ticket {
+        constructor(destination, price, status) {
+            this.destination = destination;
+            this.price = price;
+            this.status = status;
+        }
+    }
+    let unsortedTickets = [];
+
+    for (const ticket of ticketArray) {
+        let [destination, price, status] = ticket.split('|');
+        price = +price;
+        unsortedTickets.push(new Ticket(destination, price, status));
+    }
+    let sortedTickets = {};
+
+    switch (sortingCriteria) {
+        case "destination":
+            sortedTickets = unsortedTickets.sort((a, b) => {
+              return  a.destination.localeCompare(b.destination);
+            });
+            break;
+        case "price":
+            sortedTickets = unsortedTickets.sort((a, b) => {
+              return  a.price - b.price;
+            });
+            break;
+        case "status":
+            sortedTickets = unsortedTickets.sort((a, b) => {
+             return   a.status.localeCompare(b.status);
+            });
+            break;
+    }
+
+    return sortedTickets
+
+}
+console.log( solve(['Philadelphia|94.20|available',
+'New York City|95.99|available',
+'New York City|95.99|sold',
+'Boston|126.20|departed'
+],
+'destination'
+));
