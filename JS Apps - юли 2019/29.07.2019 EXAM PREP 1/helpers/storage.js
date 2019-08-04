d97a52773d504fce087393f5e3ab7ccbeb9709e8
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2019-08-04 10:22:37 +0300

    Add files via upload

---
+++ JS Apps - юли 2019/29.07.2019 EXAM PREP 1/helpers/storage.js
@@ -0,0 +1,32 @@
+const storage = function () {
+
+    const appKey = "kid_rJkYs2lmS";
+    const appSecret = "436f90fcf98840f28ceef96c472cecf1";
+
+    const getData = function (key) {
+        return localStorage.getItem(key + appKey);
+    };
+
+    const saveData = function (key, value) {
+        localStorage.setItem(key + appKey, JSON.stringify(value));
+    };
+
+    const saveUser = function (data) {
+        saveData("userInfo", data);
+        saveData("authToken", data._kmd.authtoken);
+    };
+
+    const deleteUser = function () {
+        localStorage.removeItem("userInfo" + appKey);
+        localStorage.removeItem("authToken" + appKey);
+    };
+
+    return {
+        getData,
+        saveData,
+        saveUser,
+        deleteUser,
+        appKey,
+        appSecret
+    }
+}();
